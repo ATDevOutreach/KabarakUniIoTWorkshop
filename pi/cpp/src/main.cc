60a94194f4b6a9acee48150e3ca1a1a3fa18a40a
TheBeachmaster <otienoken@outlook.com>
2018-06-19 20:50:52 +0300

    Add Mraa && MQTT library  - CPP

---
+++ pi/cpp/src/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <signal.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <thread>
+#include <chrono>
+#include "mqtt/client.h"
+#include "mraa/common.hpp"
+#include "mraa/aio.hpp"
+
